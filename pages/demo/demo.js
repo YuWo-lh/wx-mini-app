4b8bbc2f6227e0faa63292d1a634ab00d97214e5
yuwo <424576805@qq.com>
2023-03-08 17:11:46 +0800

    npm默认构建

--- pages/demo/demo.js
+++ pages/demo/demo.js
@@ -1,13 +1,3 @@
-Page({
-  data: {
-    name: '',
-    gender: ''
-  },
-  onLoad(params) {
-    console.log(params)
-    this.setData({
-      name: params.name,
-      gender: params.gender
-    })
-  }
+Page ({
+  
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,13 +1,17 @@
+// 导入构建后的 npm 模块
+const computedBehavior = require('miniprogram-computed').behavior
+
 Page ({
-  data: {
-    profile: {
-      name: 'tiger',
-      avatarUrl: '/static/images/avatar.jpg'
-    }
+  data : {
+    firstNmae: "张",
+    lastNmae: "三"
   },
-  chooseAvatar(e) {
-    this.setData({
-      'profile.avatarUrl': e.detail.avatarUrl
-    })
+  // 应用到页面中
+  behaviors: [computedBehavior],
+  // 使用计算属性
+  computed: {
+    fullName(data) {
+      return data.firstNmae + data.lastNmae
+    }
   }
 })
