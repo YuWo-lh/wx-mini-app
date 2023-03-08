aeddfd1e5ce4935ed3491c40322231c533656f4f
yuwo <424576805@qq.com>
2023-03-08 10:46:10 +0800

    day02_02

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,3 +1,19 @@
-Page ({
-  
-})
+Page({
+  data: {
+    list: [],
+  },
+  onLoad() {
+    this.getStudentList();
+  },
+  getStudentList() {
+    // wx.request.get('https://mock.boxuegu.com/mock/3293/students').then(res => console.log(res))
+    wx.request({
+      url: "https://mock.boxuegu.com/mock/3293/students",
+      method: "get",
+      success: (res) => {
+        console.log(res);
+        list: res.data.result;
+      },
+    });
+  },
+});
